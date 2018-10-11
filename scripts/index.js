96fc8c0785e0c84e13d7139665dfd6fa59b88f60
SavanMalhotra <43863307+SavanMalhotra@users.noreply.github.com>
2018-10-11 08:35:45 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,7 @@ var toolbarOptions = [
   [{ 'font': [] }],
   [{ 'align': [] }],
   ['link', 'image'],
-  ['stroke'],
-
+  
   ['clean']                                         // remove formatting button
 ];
 
