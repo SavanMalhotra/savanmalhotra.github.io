e48c7e4d776cd0632b498b2eab9d0bed33dd1322
SavanMalhotra <43863307+SavanMalhotra@users.noreply.github.com>
2018-10-10 22:35:41 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,35 @@
-var container = document.getElementById('editor');
 
-var options = {
-  debug: 'info',
-  modules: {
-    toolbar: '#toolbar'
-  },
-  placeholder: 'Compose an epic...',
-  readOnly: true,
-  theme: 'snow'
-};
-var editor = new Quill(container, options);
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+  ['link', 'image'],
+
+  ['clean']                                         // remove formatting button
+];
+
+  var quill = new Quill('#editor', {
+    debug: 'info',
+    modules: {
+      toolbar: toolbarOptions
+    },
+    placeholder: 'Compose an epic...',
+    readOnly: false,
+    theme: 'snow'
+  });
+
+document.getElementById("submitBtn").addEventListener("click", function(){
+
+});
