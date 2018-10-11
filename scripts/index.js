6173ba430f752e837b64212eba1af8acf1c7f5fc
SavanMalhotra <43863307+SavanMalhotra@users.noreply.github.com>
2018-10-10 21:45:05 -0400

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+var container = document.getElementById('editor');
+
+var options = {
+  debug: 'info',
+  modules: {
+    toolbar: '#toolbar'
+  },
+  placeholder: 'Compose an epic...',
+  readOnly: true,
+  theme: 'snow'
+};
+var editor = new Quill(container, options);
