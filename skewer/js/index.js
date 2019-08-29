3017dfc9fdee2ce7b06df6a9e8ab71918f7ad685
Savan Malhotra <savan@notifyapp.ca>
2019-08-29 15:50:49 -0400

    Added Skewer Folder

---
+++ skewer/js/index.js
@@ -0,0 +1,12 @@
+var i = 0;
+var text = 'Savan Malhotra';
+const speed = 1000;
+
+// setTimeout(displayContent, speed);
+//
+// function displayContent() {
+//   const span = document.getElementsByTagName('SPAN');
+//   console.log(span);
+//   span[0].style.display = "initial";
+//
+// }
